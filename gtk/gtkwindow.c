4fc8d01e454a4cf887ce0950c2d9f91e783c939a
Teemu Ikonen <tpikonen@mailbox.org>
2023-02-20 19:35:17 +0200

    window: Maximize resizable transient windows on small screens
    Based on Purism adaptive patches by Alexander Mikhaylenko,
    https://source.puri.sm/Librem5/debs/gtk4/-/blob/pureos/byzantium/debian/patches/pureos/adaptive/
    
    Only maximize transients on small (< 120 mm) screens

--- gtk/gtkwindow.c
+++ gtk/gtkwindow.c
@@ -33,6 +33,7 @@
 #include "gtkbuilderprivate.h"
 #include "gtkcheckbutton.h"
 #include "gtkcssshadowvalueprivate.h"
+#include "deprecated/gtkdialog.h"
 #include "gtkdroptargetasync.h"
 #include "gtkeventcontrollerlegacy.h"
 #include "gtkeventcontrollerkey.h"
@@ -65,6 +66,7 @@
 
 #include "inspector/window.h"
 
+#include "gdk/gdkmonitor.h"
 #include "gdk/gdkprofilerprivate.h"
 #include "gdk/gdksurfaceprivate.h"
 #include "gdk/gdktextureprivate.h"
@@ -3999,6 +4001,8 @@ gtk_window_map (GtkWidget *widget)
   GtkWindow *window = GTK_WINDOW (widget);
   GtkWindowPrivate *priv = gtk_window_get_instance_private (window);
   GtkWidget *child = priv->child;
+  GdkMonitor *monitor;
+  gboolean is_dialog, on_small_screen;
 
   GTK_WIDGET_CLASS (gtk_window_parent_class)->map (widget);
 
@@ -4013,6 +4017,13 @@ gtk_window_map (GtkWidget *widget)
   gtk_widget_realize_at_context (widget);
 
   gtk_window_present_toplevel (window);
+
+  /* Transient windows can be considered pseudo-dialogs. */
+  monitor = gdk_display_get_monitor_at_surface (priv->display, priv->surface);
+  on_small_screen = gdk_monitor_get_width_mm (monitor) < 120;
+  is_dialog = GTK_IS_DIALOG (widget) || !!gtk_window_get_transient_for (window);
+  if (on_small_screen && is_dialog && gtk_window_get_resizable (window))
+    gtk_window_maximize (window);
 
   if (priv->minimize_initially)
     gdk_toplevel_minimize (GDK_TOPLEVEL (priv->surface));

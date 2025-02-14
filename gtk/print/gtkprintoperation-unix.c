8ffb2d42186937623a49733e6807c87429cfb13d
Bardia Moshiri <bardia@furilabs.com>
2025-02-14 13:58:46 -0500

    gtkprintoperation-unix: force to use portal file chooser at all times
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- gtk/print/gtkprintoperation-unix.c
+++ gtk/print/gtkprintoperation-unix.c
@@ -1275,10 +1275,7 @@ _gtk_print_operation_platform_backend_run_dialog (GtkPrintOperation *op,
   else
     display = gdk_display_get_default ();
 
-  if (gdk_display_should_use_portal (display, PORTAL_PRINT_INTERFACE, 0))
-    return gtk_print_operation_portal_run_dialog (op, show_dialog, parent, do_print);
-  else
-    return gtk_print_operation_unix_run_dialog (op, show_dialog, parent, do_print);
+  return gtk_print_operation_portal_run_dialog (op, show_dialog, parent, do_print);
 }
 
 void
@@ -1294,10 +1291,7 @@ _gtk_print_operation_platform_backend_run_dialog_async (GtkPrintOperation       
   else
     display = gdk_display_get_default ();
 
-  if (gdk_display_should_use_portal (display, PORTAL_PRINT_INTERFACE, 0))
-    gtk_print_operation_portal_run_dialog_async (op, show_dialog, parent, print_cb);
-  else
-    gtk_print_operation_unix_run_dialog_async (op, show_dialog, parent, print_cb);
+  gtk_print_operation_portal_run_dialog_async (op, show_dialog, parent, print_cb);
 }
 
 void
@@ -1313,10 +1307,7 @@ _gtk_print_operation_platform_backend_launch_preview (GtkPrintOperation *op,
   else
     display = gdk_display_get_default ();
 
-  if (gdk_display_should_use_portal (display, PORTAL_PRINT_INTERFACE, 0))
-    gtk_print_operation_portal_launch_preview (op, surface, parent, filename);
-  else
-    gtk_print_operation_unix_launch_preview (op, surface, parent, filename);
+  gtk_print_operation_portal_launch_preview (op, surface, parent, filename);
 }
 
 cairo_surface_t *

b804b35a92703e2a13dd55c324194fc3ba2b063b
Yao Wei <medicalwei@gmail.com>
2025-12-19 07:30:38 +0800

    gdk/x11: check surface scale on input region, opaque region/rect
    Since in x11 backend the input region, opaque region and opaque rect are
    related to scale, it is necessary to track whether the scale is
    different so these regions can be updated without changing the logical
    size.
    
    Forwarded: https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/9017
    Bug: https://gitlab.gnome.org/GNOME/gtk/issues/7805
    Bug-Ubuntu: https://launchpad.net/bugs/1924689

--- gdk/gdksurfaceprivate.h
+++ gdk/gdksurfaceprivate.h
@@ -121,6 +121,8 @@ struct _GdkSurfaceClass
                                          double          *x,
                                          double          *y,
                                          GdkModifierType *mask);
+  gboolean     (* should_set_input_region) (GdkSurface      *surface,
+                                            cairo_region_t  *shape_region);
   void         (* set_input_region)     (GdkSurface      *surface,
                                          cairo_region_t  *shape_region);
 
@@ -154,6 +156,12 @@ struct _GdkSurfaceClass
                                            guint           *out_width,
                                            guint           *out_height);
 
+  gboolean     (* should_set_opaque_region) (GdkSurface      *surface,
+                                             cairo_region_t  *orig_region,
+                                             cairo_region_t  *region);
+  gboolean     (* should_set_opaque_rect) (GdkSurface      *surface,
+                                             cairo_rectangle_int_t  *orig_rect,
+                                             cairo_rectangle_int_t  *rect);
   void         (* set_opaque_region)      (GdkSurface      *surface,
                                            cairo_region_t *region);
   void         (* request_layout)         (GdkSurface     *surface);
@@ -356,5 +364,17 @@ GdkDrawContext *        gdk_surface_get_attached_context                (GdkSurf
 void                    gdk_surface_set_content                         (GdkSurface             *self,
                                                                          GskRenderNode          *content);
 GskRenderNode *         gdk_surface_get_content                         (GdkSurface             *self);
+
+gboolean   gdk_surface_real_should_set_input_region (GdkSurface     *surface,
+                                                     cairo_region_t *region);
+
+gboolean   gdk_surface_real_should_set_opaque_region (GdkSurface *surface,
+                                                      cairo_region_t *orig_region,
+                                                      cairo_region_t *region);
+
+gboolean   gdk_surface_real_should_set_opaque_rect (GdkSurface *surface,
+                                                    cairo_rectangle_int_t *orig_rect,
+                                                    cairo_rectangle_int_t *rect);
+
 
 G_END_DECLS
